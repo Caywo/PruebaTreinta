d9bb1e44fcc3f8a1f9aeb6fe89f21991b2f29273
TomasF <fernandezlaborde_11@hotmail.com>
2023-10-17 17:01:32 -0300

    ahora si

--- PruebaDelEntorno/src/juego/Juego.java
+++ PruebaDelEntorno/src/juego/Juego.java
@@ -67,7 +67,7 @@ this.entorno = new Entorno(this, "Perrita", 800, 600);
 			return 3;
 		}
 		return 5;
-		//PRUEBA ASL,KMDLAÑSJDLSA JKLÑASJDLPÑÁSDÑLASJDÑLSAJKDLÑASSDA
+	
 		
 	}
 	//Funcion que no permite salir del entorno
